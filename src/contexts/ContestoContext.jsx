5b3d253932abf630c3bf952f529f4d465bd648f6
Luca Lambia <luca@lambia.it>
2026-02-27 10:19:42 +0100

    minor

---
+++ src/contexts/ContestoContext.jsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, useState } from "react";
+
+const ContestoContext = createContext(null);
+
+function ContestoProvider({ children }) {
+
+	const defaultDato = {
+		key: "value"
+	};
+
+	const [dato, setDato] = useState(defaultDato);
+
+	function faiCose() {
+		console.log("Faccio cose, vedo gente");
+	}
+
+	const datiDaCondividere = { dato, setDato, faiCose };
+
+	return <ContestoContext.Provider value={datiDaCondividere}>
+		{children}
+	</ContestoContext.Provider>
+}
+
+function useContesto() {
+	return useContext(ContestoContext);
+}
+
+export { ContestoProvider, useContesto };
+
+/*************************************************/
+/**************** ESEMPI DI UTILIZZO *************/
+/*************************************************/
+
+//Providing in App:
+/*
+import { ContestoProvider } from "./contexts/ContestoContext";
+function App() {
+	return <ContestoProvider>
+		...componenti/rotte...
+	</ContestoProvider>
+*/
+
+// Utilizzo da componente:
+/*
+import { useContesto } from "../contexts/ContestoContext";
+function Componente() {
+	const { dato, setDato } = useContesto();
+	return <div>Dati ottenuti: {dato.key}</div>
+}
+*/
